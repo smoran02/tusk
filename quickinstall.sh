eb9cb7162c440e96d8f4d7dcf3ad6093475efa1e
Michael Shafae <mshafae@fullerton.edu>
2021-08-17 22:57:58 -0700

    Cleaned up the quickinstall, added extras to the quickinstall.

--- quickinstall.sh
+++ quickinstall.sh
@@ -77,6 +77,27 @@ insallfromdeb () {
     sudo rm -f ${DEB}
 }
 
+sudo_check () {
+    echo "Checking if you can execute commands with elevated priveleges."
+    echo "Enter your login password when prompted."
+    echo "NOTE: your password will be invisible. Type it carefully and slowly, then press enter."
+    sudo echo "You can run commands as root!"
+    if [ $? -ne 0 ]; then
+        echo "Sorry, you can't run commands as root. Exiting."
+        exit 1
+    fi
+}
+
+sudo_warning () {
+    echo "${1}. Elevating priveleges."
+    echo "Enter your login password if prompted."
+}
+
+
+#####
+# Main
+#####
+
 # Check ID, make sure user is not root
 ID=$(id -u)
 if [ ${ID} -eq 0 ]; then
@@ -84,19 +105,13 @@ if [ ${ID} -eq 0 ]; then
 fi
 
 # Sudo check
-echo "Checking if you can execute commands with elevated priveleges."
-echo "Enter your login password when prompted."
-echo "NOTE: your password will be invisible. Type it carefully and slowly, then press enter."
-sudo echo "You can run commands as root!"
-if [ $? -ne 0 ]; then
-    echo "Sorry, you can't run commands as root. Exiting."
-    exit 1
-fi
+sudo_check
 
 echo "Testing your network connection."
 test_dns_web
 echo "You will be downloading and installing approximately 300 MB of software. This may take some time depending on the speed of your network and the speed of your computer."
 echo "Do not shutdown or put your computer to sleep until you see your prompt again."
+
 if [ "${TUSK_NO_PROMPT}x" = "x" ]; then
     echo -n "Are you ready to continue? [y/n]"
     read answer
@@ -109,18 +124,16 @@ if [ "${TUSK_NO_PROMPT}x" = "x" ]; then
 fi
 
 # Update
-REGEX='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 
 # Updating /etc/apt/sources.list
-APT_SOURCES_HOSTURL="http://us.archive.ubuntu.com/ubuntu/"
+APT_SOURCES_HOSTURL=${TUSK_APT_SOURCES_HOSTURL:-"http://us.archive.ubuntu.com/ubuntu/"}
+
+echo "Apt archive url is ${APT_SOURCES_HOSTURL}"
 
 
-if [ "${TUSK_APT_SOURCES_HOSTURL}x" != "x" ]; then
-  echo "Overriding host url with ${TUSK_APT_SOURCES_HOSTURL}"
-  APT_SOURCES_HOSTURL=${TUSK_APT_SOURCES_HOSTURL}
-fi
+URLREGEX='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 
-if [[ $APT_SOURCES_HOSTURL =~ $REGEX ]]; then
+if [[ $APT_SOURCES_HOSTURL =~ $URLREGEX ]]; then
     CODENAME=`lsb_release -c | cut -f 2`
     ORIGINAL_APT_SOURCES=`overide_apt_sources  ${APT_SOURCES_HOSTURL} ${CODENAME}`
 else
@@ -129,19 +142,23 @@ else
 fi
 
 
-
-echo "Updating your package. Elevating priveleges."
-echo "Enter your login password if prompted."
+sudo_warning "Updating your package"
 sudo apt-get -q update
 
 # Install packages
-PACKAGES=`cat /tusk/packages.txt | sort | uniq`
+PACKAGE_SRC=${TUSK_PACKAGE_SRC:-"https://raw.githubusercontent.com/mshafae/tusk/main/packages.txt"}
+PACKAGES_RAW=`wget -O - ${PACKAGE_SRC}`
+if [ $? -ne 0 ]; then
+    echo "Could not fetch package list from ${PACKAGE_SRC}. Exiting."
+    exit 1
+fi
+
+PACKAGES=`echo ${PACKAGES_RAW} | sort | uniq`
 if [ "${PACKAGES}x" = "x" ]; then
     echo "Problem reading the packages.txt file. Exiting."
     exit 1
 fi
-echo "Installing packages defined in packages.txt."
-echo "Enter your login password if prompted."
+sudo_warning "Installing packages."
 sudo apt-get install -y ${PACKAGES}
 if [ $? -ne 0 ]; then
     echo "Could not install packages in packages.txt. Exiting."
@@ -165,6 +182,11 @@ URL="
 https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 insallfromdeb ${URL} ${DEB}
 
+# Discord
+DEB="/tmp/discord_amd64.deb"
+URL="https://discord.com/api/download?platform=linux&format=deb"
+insallfromdeb ${URL} ${DEB}
+
 # Atom - disabled
 if [ "X" = "Y" ]; then
     echo "Installing Atom"
@@ -174,17 +196,15 @@ if [ "X" = "Y" ]; then
 fi
 
 
-# GitHub client and Bazel
+# GitHub client, Bazel, VirtualBox, Vagrant
 
-echo "Adding GitHub GPG key. Elevating priveleges."
-echo "Enter login password if prompted."
+sudo_warning "Adding GitHub GPG key"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 if [ $? -ne 0 ]; then
     echo "Could not add the GitHub gpg key. Exiting."
     exit 1
 fi
-echo "Adding GitHub repository. Elevating priveleges."
-echo "Enter login password if prompted."
+sudo_warning "Adding GitHub repository"
 sudo apt-add-repository https://cli.github.com/packages
 if [ $? -ne 0 ]; then
     echo "Could not add the GitHub repository. Exiting."
@@ -205,25 +225,54 @@ if [ $? -ne 0 ]; then
     echo "Error dearmoring the key. Exiting."
     exit 1
 fi
-echo "Moving Bazel GPG key into place. Elevating priveleges."
-echo "Enter login password if prompted."
+sudo_warning "Moving Bazel GPG key into place"
 sudo mv ${BAZELDEARMOR} ${BAZELDEST}
 if [ $? -ne 0 ]; then
     echo "Error moving key into place. Exiting."
     exit 1
 fi
-echo "Creating Bazel apt source file. Elevating priveleges."
-echo "Enter login password if prompted."
+sudo_warning "Creating Bazel apt source file"
 sudo echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" > /etc/apt/sources.list.d/bazel.list
 if [ $? -ne 0 ]; then
     echo "Error creating Bazel source file. Exiting."
     exit 1
 fi
 
-echo "Installing bazel and gh. Elevating priveleges."
-echo "Enter login password if prompted."
+# Virtualbox
+sudo_warning "Adding VirtualBox GPG key"
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+if [ $? -ne 0 ]; then
+    echo "Error adding first VirtualBox GPG key. Exiting."
+    exit 1
+fi
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+if [ $? -ne 0 ]; then
+    echo "Error adding second VirtualBox GPG key. Exiting."
+    exit 1
+fi
+sudo_warning "Adding VirtualBox apt source"
+sudo apt-add-repository "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+if [ $? -ne 0 ]; then
+    echo "Error adding adding VirtualBox repository. Exiting."
+    exit 1
+fi
+
+sudo_warning "Adding Vagrant GPG key"
+wget -q https://apt.releases.hashicorp.com/gpg -O- | sudo apt-key add -
+if [ $? -ne 0 ]; then
+    echo "Error adding Vagrant GPG key. Exiting."
+    exit 1
+fi
+sudo_warning "Adding Vagrant repository"
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+if [ $? -ne 0 ]; then
+    echo "Error adding adding Vagrant repository. Exiting."
+    exit 1
+fi
+
+sudo_warning "Installing bazel, gh, virtualbox, and vagrant"
 sudo apt-get update
-sudo apt-get install -y bazel gh
+sudo apt-get install -y bazel gh virtualbox-6.1 vagrant
 
 # GTest and GMock libraries
 echo "Building Google Test and Google Mock libraries in /usr/local."
@@ -234,42 +283,51 @@ PWD=$(pwd)
 cd ${BUILDDIR}
 cmake -DCMAKE_BUILD_TYPE=RELEASE /usr/src/googletest/googlemock
 make
-echo "Creating destination directory in ${DESTROOT}. Elevating priveleges."
-echo "Enter login password if prompted."
+sudo_warning "Creating destination directory in ${DESTROOT}"
 sudo mkdir -p ${DESTROOT}/lib
 if [ $? -ne 0 ]; then
     echo "Could not create destination. Exiting."
     exit 1
 fi
-echo "Installing libraries. Elevating priveleges."
-echo "Enter login password if prompted."
-sudo install -o root -g root -m 644 ./lib/libgtest.a ${DESTROOT}/lib
-if [ $? -ne 0 ]; then
-    echo "Could not install libgtest.a. Exiting."
-    exit 1
+sudo_warning "Installing GTest and GMock libraries"
+LIBS="libgtest.a libgtest_main.a libgmock.a libgmock_main.a"
+foreach LIB ( ${LIBS} )
+    sudo install -o root -g root -m 644 ./lib/${LIB} ${DESTROOT}/lib
+    if [ $? -ne 0 ]; then
+        echo "Could not install ${LIB}. Exiting."
+        exit 1
+    fi
+done
+
+if [ "YES" = "NO" ]; then
+    sudo install -o root -g root -m 644 ./lib/libgtest.a ${DESTROOT}/lib
+    if [ $? -ne 0 ]; then
+        echo "Could not install libgtest.a. Exiting."
+        exit 1
+    fi
+
+    sudo install -o root -g root -m 644 ./lib/libgtest_main.a ${DESTROOT}/lib
+    if [ $? -ne 0 ]; then
+        echo "Could not install libgtest_main.a. Exiting."
+        exit 1
+    fi
+    sudo install -o root -g root -m 644 ./lib/libgmock.a ${DESTROOT}/lib
+    if [ $? -ne 0 ]; then
+        echo "Could not install libgmock.a. Exiting."
+        exit 1
+    fi
+
+    sudo install -o root -g root -m 644 ./lib/libgmock_main.a ${DESTROOT}/lib
+    if [ $? -ne 0 ]; then
+        echo "Could not install libgmock_main.a. Exiting."
+        exit 1
+    fi
 fi
 
-sudo install -o root -g root -m 644 ./lib/libgtest_main.a ${DESTROOT}/lib
-if [ $? -ne 0 ]; then
-    echo "Could not install libgtest_main.a. Exiting."
-    exit 1
-fi
-sudo install -o root -g root -m 644 ./lib/libgmock.a ${DESTROOT}/lib
-if [ $? -ne 0 ]; then
-    echo "Could not install libgmock.a. Exiting."
-    exit 1
-fi
-
-sudo install -o root -g root -m 644 ./lib/libgmock_main.a ${DESTROOT}/lib
-if [ $? -ne 0 ]; then
-    echo "Could not install libgmock_main.a. Exiting."
-    exit 1
-fi
 cd ${PWD}
 
 
 
 # Post install
-echo "Cleaning up! Elevating priveleges for the last time."
-echo "Enter login password if prompted."
+sudo_warning "Cleaning up!"
 apt-get clean
