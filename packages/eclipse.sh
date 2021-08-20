9b26a117b5dd230488f54000bb65d95516ba246b
Michael Shafae <mshafae@fullerton.edu>
2021-08-20 01:19:28 -0700

    Cleaned up vagrant file and vagrant directories.

---
+++ packages/eclipse.sh
@@ -0,0 +1,3 @@
+
+wget -q "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-06/R/eclipse-java-2021-06-R-linux-gtk-x86_64.tar.gz&r=1" -O /tmp/eclipse-java-2021-06-R-linux-gtk-x86_64.tar.gz
+
