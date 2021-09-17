5d79ebd42f3d60f2c5aae47514f8205f9b65d9c1
Michael Shafae <mshafae@fullerton.edu>
2021-09-17 13:30:10 -0700

    Initial add of the gitconfig fixer.

---
+++ gcf.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+#
+# Git Config Fixer
+#
+# Creates a config file that will cache your GitHub Personal Access Token
+# and set some other settings that align with daily use for a lower division
+# computer science course.
+#
+
+backup_file ()
+{
+  DATE=`date +"%Y%m%d-%S"`
+  NAME=${1}
+  NEWNAME=${NAME}-${DATE}.og
+  echo "Copying ${NAME} to ${NEWNAME}"
+  cp "$NAME" "${NEWNAME}"
+}
+
+prompt_confirm_repeat () {
+    # The prompt is $1 and the response after the response is accepted is
+    # $2 with a default value of "Thank you."
+    OUTER=true
+    RESPONSE="${2:-"Thank you."}"
+    while $OUTER; do
+        read -p "${1}" RETVAL
+        echo "You entered \"${RETVAL}\""
+        while true; do
+            read -p "Is this correct? [y/n] " YN
+            case $YN in
+                [Yy]* ) echo "${RESPONSE}"; OUTER=false; break;;
+                [Nn]* ) unset YN; break;;
+                * ) echo "Please answer y or n.";;
+            esac
+        done
+    done
+}
+
+mkgitconfig () {
+    GITCONFIG=${1:-"~/.gitconfig"}
+    NAME="${2}"
+    EMAIL="${3}"
+    NOW=`date +"%Y%m%d-%S"`
+    cat > ${GITCONFIG} <<EOF
+# Generated with gcf.sh on ${NOW}
+[user]
+        name = ${NAME}
+        email = ${EMAIL}
+[core]
+    pager = less
+    editor = gedit -w -s
+[credential]
+        helper = cache
+[pull]
+        rebase = false
+[push]
+        default = matching
+[init]
+        defaultBranch = main
+[help]
+    autocorrect = 20
+[color]
+    ui = true
+EOF
+}
+
+######
+# Main
+######
+
+GITCONFIG=${1:-"~/.gitconfig"}
+echo ${GITCONFIG}
+prompt_confirm_repeat "What is your full name? Please include your first and last name. " "Great, here's another question..."
+NAME=${RETVAL}
+
+prompt_confirm_repeat "What's your CSUF email address? " "Excellent, here's another question..."
+EMAIL=${RETVAL}
+
+if [ -e ${GITCONFIG} ]; then
+    echo "The file ${GITCONFIG} exists, so we'll make a backup by copying it to a file with the same name that ends in '.og'."
+    backup_file ${GITCONFIG}
+fi
+
+mkgitconfig ${GITCONFIG} ${NAME} ${EMAIL}
+
+echo "You're all set! Try using git and see if your credentials are cached."
