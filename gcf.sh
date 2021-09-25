dd7d7ac64d942e78fde00839d89387d0de8dfe52
/home/mshafae/.gitconfig <mshafae@fullerton.edu>
2021-09-25 00:46:48 -0700

    Added libsecret support

--- gcf.sh
+++ gcf.sh
@@ -16,6 +16,20 @@ backup_file ()
   cp "$NAME" "${NEWNAME}"
 }
 
+git_libsecret_install () {
+    LIBSECRETGIT="/usr/share/doc/git/contrib/credential/libsecret"
+    echo "If you are prompted for a password, type in the password"
+    echo "you used to login to this computer. Remember, it won't"
+    echo "print out what you type as a security precaution."
+    sudo apt-get install -y libsecret-1-0 libsecret-1-dev seahorse
+    sudo make -C ${LIBSECRETGIT}
+    if [ $? -ne 0 ]; then
+        echo "There was a problem building git's libsecret plugin. Exiting. Please report this to mshafae@fullerton.edu."
+        exit 1
+    fi
+    #git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+}
+
 prompt_confirm_repeat () {
     # The prompt is $1 and the response after the response is accepted is
     # $2 with a default value of "Thank you."
@@ -49,7 +63,7 @@ mkgitconfig () {
     pager = less
     editor = nano
 [credential]
-    helper = cache
+    helper = /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 [pull]
     rebase = false
 [push]
@@ -81,6 +95,14 @@ if [ -e ${GITCONFIG} ]; then
     backup_file ${GITCONFIG}
 fi
 
+echo
+echo "We're about to install some software and compile a few things."
+echo "This may take a second."
+echo
+
+git_libsecret_install
 mkgitconfig ${GITCONFIG} ${NAME} ${EMAIL}
 
-echo "You're all set! Try using git and see if your credentials are cached."
+echo
+echo "You're all set! Try using git to push or clone and see if your credentials are saved."
+echo "If you need to add or remove your GitHub password use the program named 'Passwords and Keys' and click 'Login', look for https://@github.com. Right click on it and you can edit the your saved GitHub password."
