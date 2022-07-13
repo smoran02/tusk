f8f28c01f433c61a32619330bbb4fbbb41269333
Michael Shafae <mshafae@fullerton.edu>
2022-07-13 01:34:23 -0700

    Changed default of TUSK_APT_SOURCES_OVERRIDE to NO

--- quickinstall.sh
+++ quickinstall.sh
@@ -17,9 +17,7 @@
 # TUSK_APT_SOURCES_OVERRIDE
 #   Set to "YES" to override the default apt sources list with custom URL
 #   (see TUSK_APT_SOURCES_HOSTURL) or with additional repositories.
-#   The value is YES if the distribution is Ubuntu, otherwise
-#   it is NO. Setting it to NO in the environment prior to execution will
-#   set it NO.
+#	The default value is NO
 # TUSK_APT_SOURCES_HOSTURL
 #   The URL used for apt; used in conjunction with TUSK_APT_SOURCES_OVERRIDE
 #   The default value is http://us.archive.ubuntu.com/ubuntu/
@@ -188,7 +186,7 @@ distribution_check () {
     DIST=`lsb_release -d  | awk {'first = $1; $1=""; gsub("^ ", ""); print $0'}`
     if echo ${DIST} | grep "Ubuntu" > /dev/null 2>&1; then
         DISTRIBUTION="Ubuntu"
-        TUSK_APT_SOURCES_OVERRIDE=${TUSK_APT_SOURCES_OVERRIDE:-"YES"}
+        TUSK_APT_SOURCES_OVERRIDE=${TUSK_APT_SOURCES_OVERRIDE:-"NO"}
     elif echo ${DIST} | grep "Mint" > /dev/null 2>&1; then
         DISTRIBUTION="Mint"
         export TUSK_APT_SOURCES_OVERRIDE="NO"
