4bf649025d646bc28b2f35fd3f903a4760c1dd3a
Michael Shafae <mshafae@fullerton.edu>
2022-08-30 19:46:08 -0700

    Made gcf.sh fancier.

--- gcf.sh
+++ gcf.sh
@@ -16,22 +16,78 @@ backup_file ()
   cp "$FILENAME" "${NEWFILENAME}"
 }
 
-git_libsecret_install () {
-    LIBSECRETGIT="/usr/share/doc/git/contrib/credential/libsecret"
+sudo_check () {
+    export GCF_CAN_SUDO="NO"
+    echo "We are going to check to see if you can run commands as root."
+    echo "The root user is the administrator and sometimes you may not"
+    echo "have access to programs like sudo and su which allow you to run"
+    echo "commands as root."
     echo "If you are prompted for a password, type in the password"
     echo "you used to login to this computer. Remember, it won't"
     echo "print out what you type as a security precaution."
-    sudo apt-get update
-    echo "Updated your packages, let's install some packages..."
-    sudo apt-get install -y libsecret-1-0 libsecret-1-dev seahorse
-    echo "Great, the packages were installed. Let's build libsecret..."
-    sudo make -C ${LIBSECRETGIT}
-    if [ $? -ne 0 ]; then
-        echo "There was a problem building git's libsecret plugin. Exiting. Please report this to mshafae@fullerton.edu."
-        exit 1
+    sudo -l
+    if [ $? -eq 0 ]; then
+        echo
+        echo "***********************"
+        echo "* YOU HAVE THE POWER! *"
+        echo "***********************"
+        echo "You can run commands as root. Don't abuse this power."
+        export GCF_CAN_SUDO="YES"
+    else
+        echo
+        echo "Sorry, Charlie. You can't run commands as root."
     fi
-    echo "Excellent, we've got all the parts ready to go."
-    #git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+}
+
+git_libsecret_install () {
+    LIBSECRETGIT="/usr/share/doc/git/contrib/credential/libsecret"
+    PREREQ="git make gcc libglib2.0-dev"
+    HAS_PREREQ="YES"
+    echo "Checking for prerequisite packages before building libsecret..."
+    for P in ${PREREQ}; do
+        dpkg-query -W -f='${Package} ${Status} ${Version}\n' ${P} > /dev/null
+        if [ $? -ne 0 ]; then
+            HAS_PREREQ="NO"
+            break
+        fi
+    done
+    if [ ${HAS_PREREQ}"x" = "NOx" ]; then
+        echo "You're missing one or more critical packages, so this program can't"
+        echo "continue. Most likely, you are missing all the build tools."
+        echo "To fix this problem first install the build tools and then rerun"
+        echo "this script."
+        echo "Try running:"
+        echo "wget -q https://raw.githubusercontent.com/mshafae/tusk/main/quickinstall.sh -O- | sh"
+        echo "This will install all the software you need for writing C++ programs."
+        echo "Exiting...git is not configured."
+        exit 1
+    else
+        echo "If you are prompted for a password, type in the password"
+        echo "you used to login to this computer. Remember, it won't"
+        echo "print out what you type as a security precaution."
+        PACKAGES="libsecret-1-0 libsecret-1-dev seahorse"
+        MISSING_PACKAGES="NO"
+        for P in ${PACKAGES}; do
+            dpkg-query -W -f='${Package} ${Status} ${Version}\n' ${P} > /dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                MISSING_PACKAGES="YES"
+                break
+            fi
+        done
+        if [ ${MISSING_PACKAGES}"x" = "YESx" ]; then
+            sudo apt-get update
+            echo "Updated your packages, let's install some packages..."    
+            sudo apt-get install -y ${PACKAGES}
+        fi
+        echo "Great, the packages were installed. Let's build libsecret..."
+        sudo make -C ${LIBSECRETGIT}
+        if [ $? -ne 0 ]; then
+            echo "There was a problem building git's libsecret plugin. Exiting. Please report this to mshafae@fullerton.edu."
+            exit 1
+        fi
+        echo "Excellent, we've got all the parts ready to go."
+        git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+    fi
 }
 
 prompt_confirm_repeat () {
@@ -66,8 +122,6 @@ mkgitconfig () {
 [core]
     pager = less
     editor = nano
-[credential]
-    helper = /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 [pull]
     rebase = false
 [push]
@@ -99,14 +153,25 @@ if [ -e ${GITCONFIG} ]; then
     backup_file ${GITCONFIG}
 fi
 
-echo
-echo "We're about to install some software and compile a few things."
-echo "This may take a minute."
-echo
-
-git_libsecret_install
 echo "Let's write out a new git configuration."
 mkgitconfig "${GITCONFIG}" "${NAME}" "${EMAIL}"
+
+echo "Let's see if we can cache your PAT..."
+sudo_check
+
+if [ ${GCF_CAN_SUDO}"x" = "YESx" ]; then
+    echo
+    echo "We're about to install some software and compile a few things."
+    echo "This may take a minute."
+    echo
+    git_libsecret_install
+else
+    echo "Since you can't run commands as root, we can't install the"
+    echo "software which will save your GitHub PAT. This is a bummer."
+    echo "You still have a correctly configured git client, you will"
+    echo "just have to copy and paste your PAT."
+fi
+
 
 echo
 echo "You're all set! Try using git to push or clone and see if your credentials are saved."
