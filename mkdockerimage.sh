03028176ad92bd6f924a96d5c286a614240554fd
Michael Shafae <mshafae@fullerton.edu>
2023-03-03 11:20:23 -0800

    Forgot the -y

--- mkdockerimage.sh
+++ mkdockerimage.sh
@@ -22,7 +22,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo debootstrap ${DIST} ${TARGET}
 
