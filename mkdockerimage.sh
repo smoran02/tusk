4846fc475d7f8da2c681348934ab2e99125a5fb7
Michael Shafae <mshafae@fullerton.edu>
2023-03-03 20:50:07 -0800

    Cleaned up and verified that it delivers.

--- mkdockerimage.sh
+++ mkdockerimage.sh
@@ -4,7 +4,7 @@
 # Installs via tusk's quickinstall.sh and adds docker for the image building.
 # Output is ${TARGET}.tar.gz in the CWD.
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
     echo "Provide an ubuntu dist code name like focal or jammy."
     exit 1
 fi
@@ -13,18 +13,18 @@ DIST=$1
 TARGET="tusk-${DIST}"
 
 
-sudo apt-get update
-
-sudo apt-get install -y debootstrap ca-certificates curl gnupg lsb-release
-
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# sudo apt-get update
+#
+# sudo apt-get install -y debootstrap ca-certificates curl gnupg lsb-release
+#
+# sudo mkdir -m 0755 -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+#
+# sudo apt-get update
+#
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo debootstrap ${DIST} ${TARGET}
 
