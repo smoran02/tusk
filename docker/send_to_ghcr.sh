a33e01400ebef90085ef6c2b9d4c216bd7dfa7de
Michael Shafae <mshafae@fullerton.edu>
2023-08-24 23:49:48 -0700

    Script to send containers to GHCR.

---
+++ docker/send_to_ghcr.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+if [ "${MS_GITHUB_PAT}x" -eq "x" ]; then
+    echo "You need to set MS_GITHUB_PAT with your GH PAT to continue."
+    exit 1
+fi
+
+echo $MS_GITHUB_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+ID=$(docker image ls  mshafae/tusk-jammy-min:latest | tail +2 | awk '{print $3}')
+DATE=$(docker image ls --format "table {{.ID}} {{.Repository}} {{.Tag}}" | grep $ID | grep -v latest | awk '{print $3}')
+
+docker tag ${ID} ghcr.io/mshafae/${TARGET}:${DATE}
+docker tag ${ID} ghcr.io/mshafae/${TARGET}:latest
+
+docker push ghcr.io/mshafae/${TARGET}:${DATE}
+docker push ghcr.io/mshafae/${TARGET}:latest
