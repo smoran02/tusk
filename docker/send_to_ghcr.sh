798f8fab722496c94bd1ede8c20a5eb4f059e200
Michael Shafae <mshafae@fullerton.edu>
2023-08-25 01:00:15 -0700

    Added a note.

--- docker/send_to_ghcr.sh
+++ docker/send_to_ghcr.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# Inspired by https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry
+
 if [ "${MS_GITHUB_PAT}x" -eq "x" ]; then
     echo "You need to set MS_GITHUB_PAT with your GH PAT to continue."
     exit 1
