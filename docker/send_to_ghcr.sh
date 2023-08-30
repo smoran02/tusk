0bbdd5d441a56581dd0f809e7478c39c81510801
Michael Shafae <mshafae@fullerton.edu>
2023-08-29 17:56:38 -0700

    Fixed clang++ install.

--- docker/send_to_ghcr.sh
+++ docker/send_to_ghcr.sh
@@ -2,7 +2,7 @@
 
 # Inspired by https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry
 
-if [ "${MS_GITHUB_PAT}x" -eq "x" ]; then
+if [ "${MS_GITHUB_PAT}x" = "x" ]; then
     echo "You need to set MS_GITHUB_PAT with your GH PAT to continue."
     exit 1
 fi
@@ -11,7 +11,9 @@ echo $MS_GITHUB_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
 ID=$(docker image ls  mshafae/tusk-jammy-min:latest | tail +2 | awk '{print $3}')
 DATE=$(docker image ls --format "table {{.ID}} {{.Repository}} {{.Tag}}" | grep $ID | grep -v latest | awk '{print $3}')
-
+echo $ID
+echo $DATE
+exit 1
 docker tag ${ID} ghcr.io/mshafae/${TARGET}:${DATE}
 docker tag ${ID} ghcr.io/mshafae/${TARGET}:latest
 
