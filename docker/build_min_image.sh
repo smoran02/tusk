ec9399e1ba7dde1b1ee1971cee3757084cc40052
Michael Shafae <mshafae@fullerton.edu>
2023-09-19 20:58:05 -0700

    Added lunar and made things more generic.

--- docker/build_min_image.sh
+++ docker/build_min_image.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/bash
 
-TARGET="tusk-jammy-min"
+RELEASES="jammy lunar"
+TARGET="tusk"
 DATE=$(date "+%Y%m%d%H%M")
 
-ID=$(docker build -q -t ${TARGET} -f Dockerfile-min  .)
-# ID=$(docker image ls | grep ${TARGET}  | awk '{print $3}')
+for REL in ${RELEASES}; do
+    CURRENTTARGET="${TARGET}-${REL}-min"
+    echo ${CURRENTTARGET}
+    ID=$(docker build -q -t ${CURRENTTARGET} -f Dockerfile-${REL}-all  .)
+    # ID=$(docker image ls | grep ${TARGET}  | awk '{print $3}')
 
-docker tag ${ID} mshafae/${TARGET}:${DATE}
-docker tag ${ID} mshafae/${TARGET}:latest
+    docker tag ${ID} mshafae/${CURRENTTARGET}:${DATE}
+    docker tag ${ID} mshafae/${CURRENTTARGET}:latest
 
-docker push mshafae/${TARGET}:${DATE}
-docker push mshafae/${TARGET}:latest
+    docker push mshafae/${CURRENTTARGET}:${DATE}
+    docker push mshafae/${CURRENTTARGET}:latest
 
-echo "To Test"
-echo "docker run -it --user tuffy ${TARGET}"
+    echo "To Test"
+    echo "docker run -it --user tuffy ${CURRENTTARGET}"
+done
