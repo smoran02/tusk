3b8d046e799b078610a9f4e79aa360a4afab2240
Michael Shafae <mshafae@fullerton.edu>
2024-08-24 15:12:30 -0700

    Push all containers to github.

--- docker/send_to_ghcr.sh
+++ docker/send_to_ghcr.sh
@@ -28,4 +28,22 @@ for REL in ${RELEASES}; do
         echo "Couldn't find the right ID for ${TARGET}"
         exit 1
     fi
+
+    TARGET="tusk-${REL}"
+    ID=$(docker image ls  mshafae/${TARGET}:latest | tail +2 | awk '{print $3}')
+    if [ $ID ]; then
+	DATE=$(docker image ls --format "table {{.ID}} {{.Repository}} {{.Tag}}" | grep $ID | grep -v latest | awk '{print $3}' | head -1)
+        echo $ID
+        echo $DATE
+        #exit 1
+        docker tag ${ID} ghcr.io/mshafae/${TARGET}:${DATE}
+        docker tag ${ID} ghcr.io/mshafae/${TARGET}:latest
+
+        docker push ghcr.io/mshafae/${TARGET}:${DATE}
+        docker push ghcr.io/mshafae/${TARGET}:latest
+    else
+        echo "Couldn't find the right ID for ${TARGET}"
+        exit 1
+    fi
+
 done
